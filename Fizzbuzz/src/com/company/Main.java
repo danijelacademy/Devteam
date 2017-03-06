530b445d804355ce6a264f5fda9b9d8c2b58af64
Matilda Bergman <matilda.bergman1@gmail.com>
2017-03-06 10:09:05 +0100

    :add
    comEEGSDG1IJ

---
+++ Fizzbuzz/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	for (int i=0; i<=100; i++){
+	    if (i%3==0 && i%5==0)
+	        System.out.println("Fizzbuzz");
+
+
+
+
+        }
+    }
+}
